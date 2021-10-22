4999db0fc32bebf5dee4419f74d9a69585bb1aa7
desjoerd <sjoerd.meer@outlook.com>
2021-10-22 22:03:19 +0200

    Initial commit with baby steps

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+
+# oh-my-posh (nice prompts)
+# see https://ohmyposh.dev/
+oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
---
+++ install.ps1
@@ -0,0 +1,6 @@
+# Install Fonts
+Start-Process 'https://www.nerdfonts.com/font-downloads'
+
+# Install OhMyPosh
+winget install -s winget -i JanDeDobbeleer.OhMyPosh
+Start-Process 'https://ohmyposh.dev/'
