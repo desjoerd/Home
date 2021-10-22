cf08a4c4b328c94e92b850cc5b69a9ad32d6d661
desjoerd <sjoerd.meer@outlook.com>
2021-10-22 23:55:44 +0200

    more settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 
 # oh-my-posh (nice prompts)
 # see https://ohmyposh.dev/
-oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:\D\repos\github\desjoerd\home\jandedobbeleer.omp.desjoerd.json | Invoke-Expression
