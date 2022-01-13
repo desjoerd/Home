bf7e427ae6ba699493fcae63c3f33fc72ad9823c
desjoerd <sjoerd.meer@outlook.com>
2022-01-13 22:54:34 +0100

    Add ssms, azure cli and jetbrains

--- install.ps1
+++ install.ps1
@@ -3,12 +3,12 @@ Start-Process 'https://www.nerdfonts.com/font-downloads'
 
 
 # Install OhMyPosh
-winget install --exact --source winget --id JanDeDobbeleer.OhMyPosh
+winget install --exact --source winget --id JanDeDobbeleer.OhMyPosh --interactive
 Start-Process 'https://ohmyposh.dev/'
 
 # Management studio
-winget install --exact --source winget --id Microsoft.SQLServerManagementStudio
-winget install --exact --source winget --id Microsoft.AzureCLI
+winget install --exact --source winget --id Microsoft.SQLServerManagementStudio --interactive
+winget install --exact --source winget --id Microsoft.AzureCLI --interactive
 
 # JetBrains
-winget install --exact --source winget --id JetBrains.Toolbox
+winget install --exact --source winget --id JetBrains.Toolbox --interactive
