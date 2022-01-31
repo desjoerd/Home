eae8a9689740fae37ec088c154abf7c39ee3060a
desjoerd <sjoerd.meer@outlook.com>
2022-01-31 09:47:40 +0100

    Update install script and ohmyposh config

--- install.ps1
+++ install.ps1
@@ -1,14 +1,25 @@
 # Install Fonts
 Start-Process 'https://www.nerdfonts.com/font-downloads'
 
+winget install --exact --source winget --interactive --id Microsoft.WindowsTerminal
+
+winget install --exact --source winget --interactive --id AgileBits.1Password
+winget install --exact --source winget --interactive --id Git.Git
+winget install --exact --source winget --interactive --id OpenJS.NodeJS
+winget install --exact --source winget --interactive --id Microsoft.PowerShell
+winget install --exact --source winget --interactive --id Microsoft.VisualStudioCode
+winget install --exact --source winget --interactive --id Microsoft.VisualStudio.2022.Enterprise
+winget install --exact --source winget --interactive --id Microsoft.SQLServerManagementStudio
+winget install --exact --source winget --interactive --id Microsoft.AzureCLI
+winget install --exact --source winget --interactive --id JetBrains.Toolbox
+winget install --exact --source winget --interactive --id 7zip.7zip
+winget install --exact --source winget --interactive --id SlackTechnologies.Slack
+winget install --exact --source winget --interactive --id Discord.Discord
+winget install --exact --source winget --interactive --id Spotify.Spotify
+
+# Install Fonts
+Start-Process 'https://www.nerdfonts.com/font-downloads'
 
 # Install OhMyPosh
 winget install --exact --source winget --id JanDeDobbeleer.OhMyPosh --interactive
 Start-Process 'https://ohmyposh.dev/'
-
-# Management studio
-winget install --exact --source winget --id Microsoft.SQLServerManagementStudio --interactive
-winget install --exact --source winget --id Microsoft.AzureCLI --interactive
-
-# JetBrains
-winget install --exact --source winget --id JetBrains.Toolbox --interactive
